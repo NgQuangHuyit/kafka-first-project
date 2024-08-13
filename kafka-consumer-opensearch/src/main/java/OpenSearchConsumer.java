f8ab40e9826ce9764e3e65d7d5f4a784f7822ef5
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2024-08-13 23:41:02 +0700

    update OpenSearchConsumer

--- kafka-consumer-opensearch/src/main/java/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/OpenSearchConsumer.java
@@ -1,3 +1,4 @@
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -7,6 +8,9 @@ import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
@@ -63,7 +67,7 @@ public class OpenSearchConsumer {
         props.put("group.id", groupId);
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("auto.offset.reset", "latest");
+        props.put("auto.offset.reset", "earliest");
         return new KafkaConsumer(props);
     }
 
@@ -71,6 +75,18 @@ public class OpenSearchConsumer {
         RestHighLevelClient openSearchClient = createOpenSearchClient();
         Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
         KafkaConsumer consumer = createKafkaConsumer();
+
+        final Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Shutting down");
+            consumer.wakeup();
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
 
         try (openSearchClient; consumer) {
             boolean indexExists = openSearchClient
@@ -91,18 +107,52 @@ public class OpenSearchConsumer {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
                 int cnt = records.count();
                 log.info("Consumed " + cnt + " records");
+                BulkRequest bulkRequest = new BulkRequest();
+
                 for (ConsumerRecord<String, String> record : records) {
                     try {
+                        String id = extractId(record.value());
                         IndexRequest indexRequest = new IndexRequest("wikimedia")
-                                .source(record.value(), XContentType.JSON);
-                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
-                        log.info(response.getId());
-                    } catch (IOException e) {
+                                .source(record.value(), XContentType.JSON)
+                                .id(id);
+                    //    IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        bulkRequest.add(indexRequest);
+                    } catch (Exception e) {
                         log.error(e.getMessage());
                     }
                 }
+                if (bulkRequest.numberOfActions() > 0) {
+                    BulkResponse response = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+                    log.info("inserted batch with" + response.getItems().length + "records");
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+                consumer.commitSync();
+                log.info("committed sync");
             }
+        } catch (WakeupException e) {
+            log.info("Wakeup Exception");
+        } catch (Exception e) {
+            log.error("Exception in main thread", e);
+            e.printStackTrace();
+        } finally {
+            consumer.close();
+            openSearchClient.close();
+            log.info("Shutdown Complete");
         }
+
+    }
+
+    private static String extractId(String jsonString) {
+        return JsonParser.parseString(jsonString)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
 
     }
 }
