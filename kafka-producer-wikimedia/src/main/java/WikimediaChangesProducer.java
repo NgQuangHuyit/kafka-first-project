c9e5182493662c0157c7993613c79d82c9ed5e22
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2024-08-13 16:54:32 +0700

    update: update producer config

--- kafka-producer-wikimedia/src/main/java/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/WikimediaChangesProducer.java
@@ -2,6 +2,8 @@ import com.launchdarkly.eventsource.EventSource;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.RoundRobinPartitioner;
 
 import java.net.URI;
 import java.util.Properties;
@@ -16,6 +18,20 @@ public class WikimediaChangesProducer {
         props.put("bootstrap.servers", bootstrapServers);
         props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        //set safe producer config
+
+        props.setProperty("acks", "all");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+        props.put(ProducerConfig.LINGER_MS_CONFIG, Integer.toString(10));
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+
+        props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, RoundRobinPartitioner.class.getName());
+
+
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
